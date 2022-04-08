555c041102aeccd7b3bb18cd93444acc6321f84d
axhaar <pranshumishra2001@gmail.com>
2022-04-08 22:14:00 +0530

    added custom images

--- game.py
+++ game.py
@@ -20,9 +20,9 @@ GREEN = (0,255,0)
 BLUE = (0,0,255)
 
 gameDisplay.fill((BLACK))
-background = pygame.image.load('back.jpg')                                
+background = pygame.image.load('images/background.jpg')                                
 font = pygame.font.Font(os.path.join(os.getcwd(), 'font.otf'), 42)
 score_text = font.render('Score : ' + str(score), True, (255, 255, 255))   
-lives_icon = pygame.image.load('white_lives.png')   
+lives_icon = pygame.image.load('images/white_lives.png')   
 
 pygame.quit()
