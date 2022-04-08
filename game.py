8b53fe9f463b69d96874250431458b58be909f36
axhaar <pranshumishra2001@gmail.com>
2022-04-08 22:11:59 +0530

    setting the stage for window

--- game.py
+++ game.py
@@ -2,6 +2,10 @@ import pygame, sys
 import os
 import random
 
+player_lives = 3                                                
+score = 0                                                       
+fruits = ['melon', 'orange', 'pomegranate', 'guava', 'bomb']    
+
 WIDTH = 800
 HEIGHT = 500                                                
 pygame.init()
@@ -14,3 +18,11 @@ BLACK = (0,0,0)
 RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
+
+gameDisplay.fill((BLACK))
+background = pygame.image.load('back.jpg')                                
+font = pygame.font.Font(os.path.join(os.getcwd(), 'font.otf'), 42)
+score_text = font.render('Score : ' + str(score), True, (255, 255, 255))   
+lives_icon = pygame.image.load('white_lives.png')   
+
+pygame.quit()
