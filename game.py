1c5188b88bbefe2be5457602cc455dee6ebbed31
axhaar <pranshumishra2001@gmail.com>
2022-04-08 22:17:20 +0530

    function generate_random_fruits implemented

--- game.py
+++ game.py
@@ -23,6 +23,26 @@ gameDisplay.fill((BLACK))
 background = pygame.image.load('images/background.jpg')                                
 font = pygame.font.Font(os.path.join(os.getcwd(), 'font.otf'), 42)
 score_text = font.render('Score : ' + str(score), True, (255, 255, 255))   
-lives_icon = pygame.image.load('images/white_lives.png')   
+lives_icon = pygame.image.load('images/white_lives.png')
+
+def generate_random_fruits(fruit):
+    fruit_path = "images/" + fruit + ".png"
+    data[fruit] = {
+        'img': pygame.image.load(fruit_path),
+        'x' : random.randint(100,500),               
+        'y' : 800,
+        'speed_x': random.randint(-10,10),    
+        'speed_y': random.randint(-80, -60),    
+        'throw': False,                       
+        't': 0,                               
+        'hit': False,
+    }
+    if random.random() >= 0.75:     
+        data[fruit]['throw'] = True
+    else:
+        data[fruit]['throw'] = False
+data = {}
+for fruit in fruits:
+    generate_random_fruits(fruit)
 
 pygame.quit()
